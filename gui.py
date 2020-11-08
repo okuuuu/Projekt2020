024ab0f3ccb4c3e80632efe19e797ce74c9dcfcb
okuuuu <73139441+okuuuu@users.noreply.github.com>
2020-11-08 14:40:11 +0200

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from yhik import yhikud
+
+def fcn2():
+    return
+def fcn3():
+    return
+
+class Kalk:
+    def __init__(self,txt,wd,ht,rw,cm, CMD):
+        self = Button(text = txt, width = wd, height = ht, command = CMD)
+        self.grid(row=rw,column=cm)
+
+root = Tk()
+root.title("LTKalk")
+
+Welcome = Label(root, text="Püütoni loodusteaduse\nkalkulaator", width = 48, height = 5, relief=GROOVE)
+Welcome.grid(row=0,column=0,columnspan=3)
+
+Kalk1 = Kalk("Kalk1", 16, 3, 1, 0, yhikud)
+Kalk2 = Kalk("Kalk2", 16, 3, 1, 1, fcn2)
+Kalk3 = Kalk("Kalk3", 16, 3, 1, 2, fcn3)
+
+root.mainloop()
