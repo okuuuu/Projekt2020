a717022a29b1c2a177448ad4059a6cbfaf71cd89
Tomi Kuusik <tomikuusik@gmail.com>
2020-11-10 00:46:59 +0200

    nüüd saab valemeid lisada ja eemaldada

--- kalkulaator.py
+++ kalkulaator.py
@@ -1,3 +1,4 @@
+import pickle
 class Valem:
     def __init__(self, nimi, muutujad, valemid):
         self.nimi = nimi
@@ -22,18 +23,55 @@ def opereeri(usr_valem, usr_var, arvuta):
         valem = str(eval(valem))
     print(usr_valem.muutujad[usr_var] + "= " + valem)
 
+
+def lisa_valem():
+    muutujad = []
+    muutujate_avaldused = []
+    nimi = input("Mis on valemi nimi? ")
+    x = int(input("Mitu erinevat muutujat on valemis? "))
+    for i in range(x):
+        muutujad.append(input("Sisesta " + str(i+1) + ". muutuja tähistus: "))
+    print("Kasutades Pythoni syntaxi, kuidas vastavad muutujad avalduvad: (" + ",".join(muutujad) + ")")
+    for i in range(x):
+        muutujate_avaldused.append(input(muutujad[i] + "= "))
+    kõik_valemid.append(Valem(nimi, muutujad, muutujate_avaldused))
+    with open("valemid.pickle", "wb") as f:
+        pickle.dump(kõik_valemid, f)
+
+def eemalda_valem():
+    print("Kõik valemid on järgmised")
+    for i, x in enumerate(kõik_valemid):
+        print(str(i+1) + ". " + x.nimi)
+    del kõik_valemid[int(input("Millist soovite eemaldada, sisestage jrk nr - "))-1]
+    with open("valemid.pickle", "wb") as f:
+        pickle.dump(kõik_valemid, f)
+
 def main():
+    global kõik_valemid
+    try:
+        with open("valemid.pickle", "rb") as f:
+            kõik_valemid = pickle.load(f)
+    except:
+        with open("valemid.pickle", "wb") as f:
+            pickle.dump(kõik_valemid, f)
     print("Tere!")
     print("See kalkulaator suudab viib valemi õigele kujule kui sisestate vastavad muutujad")
     print("Hetkel toetame vastavaid valemarvutusi:\n")
     for i, x in enumerate(kõik_valemid):
         print(str(i+1) + ". " + x.nimi + " " + x.muutujad[0] + "=" + x.valemid[0])
-    usr_valem = kõik_valemid[int(input("\nValige vastav valem: "))-1]
-    usr_var = usr_valem.muutujad.index(input("Millise muutuja kaudu soovite valemi avaldada? "))
+    print("\n")
+    if input("-- Kas soovite valemeid lisada (y/n) - ") == "y":
+        lisa_valem()
+        print("\n\n\n")
+        return main()
+    elif input("-- Kas soovite valemeid eemaldada (y/n) - ") == "y":
+        eemalda_valem()
+        print("\n\n\n")
+        return main()
+    usr_valem = kõik_valemid[int(input("-- Millise valemiga soovite opereerida? (jrk nr) - "))-1]
+    usr_var = usr_valem.muutujad.index(input("-- Millise muutuja kaudu soovite valemi avaldada (" + ",".join(usr_valem.muutujad) + ") - "))
 
-    operation = True if int(input("\nKuvan lihtsalt valemi = 1\nArvutan kindlate väärtustega = 2\n\nValik: ")) == 2 else False
+    arvutan = True if input("-- Arvutan tulemusi koos kindlate muutujate väärtustega? (y/n) - ") == "y" else False
 
-    opereeri(usr_valem, usr_var, operation)
-
+    opereeri(usr_valem, usr_var, arvutan)
 main()
-
